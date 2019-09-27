41e6c22744e65dcad7096f58085f77365857b62b
RogerTorresA <55524395+RogerTorresA@users.noreply.github.com>
2019-09-26 20:22:42 -0500

    Update PRACTICA1.m

--- PRACTICA1.m
+++ PRACTICA1.m
@@ -1,11 +1,11 @@
-xl=0
-xu=2
-i=1
-e(i)= 10
+xl=0;
+xu=2;
+i=1;
+e(i)= 10;
 
 while(e(i)>0.01)
     
-xr=(xl+xu)/2
+xr=(xl+xu)/2;
 
 g_xl = xl^2-2;
 g_xr = xr^2-2;
@@ -13,12 +13,12 @@ g_xu = xu^2-2;
 
 
 if(g_xl*g_xr < 0 )
-    xu = xr
+    xu = xr;
     e(i+1) = abs(xr-xl);
 end
 
 if (g_xu*g_xr < 0 )
-    xl = xr
+    xl = xr;
     e(i+1) = abs(xu -xr);
 end
 i = i+1;
